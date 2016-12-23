2e9f7cf95f0842a4e5c0638f0b0dec30dcaa7970
Ethan Oblak <e@ethanoblak.com>
2016-12-22 19:58:16 -0800

    added filtering

---
+++ docs/js/rtfinlist.js
@@ -0,0 +1,15 @@
+var options = {
+  valueNames: ['reference']
+};
+
+var pubList = new List('publications', options);
+var nfbList = new List('neurofeedback', options);
+var cliList = new List('clinical', options);
+var bciList = new List('bci', options);
+var revList = new List('review', options);
+var metList = new List('methods', options);
+var fmrList = new List('fmri', options);
+var eegList = new List('eeg', options);
+var fniList = new List('fnirs', options);
+var megList = new List('meg', options);
+var mixList = new List('mixed', options);
